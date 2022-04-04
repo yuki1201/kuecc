2b09b787df88a5f9ad40f689e622deebe0ab2239
yuki1201 <yuki12017825@gmail.com>
2022-04-04 19:36:23 +0900

    add fn of add and sub

--- ycc.c
+++ ycc.c
@@ -6,11 +6,27 @@ int main(int argc, char **argv){
 		fprintf(stderr,"arguments number error");
 		return 1;
 	}
+	char *p = argv[1];
 
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
-	printf("	mov rax, %d\n",atoi(argv[1]));
+	printf("	mov rax, %ld\n",strtol(p,&p,10));
+
+	while(*p){
+		if (*p!='+'&&*p!='-'){
+			fprintf(stderr,"unexpexted character:'%c'\n",*p);
+			return 1;
+		}
+		if(*p=='+'){
+			printf("	add ");
+		}
+		if(*p=='-'){
+			printf("	sub ");
+		}
+		p++;
+		printf("rax, %ld\n", strtol(p,&p,10));
+	}
 	printf("	ret\n");
 	return 0;
 }
